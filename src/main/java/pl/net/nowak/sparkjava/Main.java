c5ad0db0f011a2ab4fd4ab1ad0b3d1561372eaf7
maciek <maku17@gmail.com>
2015-01-10 15:29:38 +0100

    Pierwszy commit

---
+++ src/main/java/pl/net/nowak/sparkjava/Main.java
@@ -0,0 +1,40 @@
+package pl.net.nowak.sparkjava;
+
+import spark.*;
+
+import java.util.logging.Logger;
+
+import static spark.Spark.get;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maciek
+ * Date: 10.01.15
+ * Time: 14:56
+ */
+public class Main {
+
+    private static Logger log =Logger.getLogger(Main.class.getCanonicalName());
+
+    public static void main(String[] args) {
+        get("/hello/:name", (req, res) -> {
+                log.info(req.params(":name"));
+                return "Hello World" + req.params(":name");
+        });
+
+
+        get("/",new Route() {
+            @Override
+            public ModelAndView handle(Request request, Response response) {
+                return new ModelAndView("hello","");
+            }
+        }, new ResponseTransformer() {
+                    @Override
+                    public String render(Object o) throws Exception {
+                        return String.valueOf(((ModelAndView)o).getModel());
+                    }
+                });
+
+    }
+
+}
